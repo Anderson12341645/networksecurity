a42a6fe278b9e41c537f88f9c57264847797845f
Anderson12341645 <akfloot@gmail.com>
2025-07-31 15:52:09 -0300

    Data ingestion completed

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionArtifact:
+    """
+    Data class for storing the artifact of data ingestion.
+    """
+    trained_file_path: str
+    test_file_path: str
