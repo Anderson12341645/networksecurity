62d71509323d2bb58febe06784c0adfb4aa86ba2
Anderson12341645 <akfloot@gmail.com>
2025-08-05 15:17:11 -0300

    The first commit

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -20,3 +20,11 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    """Data class for storing the artifact of data transformation.
+    """
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+    transformed_object_file_path: str
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,28 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+    
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save a numpy array to a file.
+    file_path: str - The path where the numpy array will be saved.
+    array: np.ndarray - The numpy array to be saved.
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e  
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info(f"Saving object at: {file_path}")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info(f"Object saved at: {file_path}")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
